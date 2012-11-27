e8954c59fd20139941a19d4c655ed315ca47cc3d
Brian Crapo <crapoblog@gmail.com>
2012-11-26 19:32:41 -0700

    Created a rails app (again) and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breadapp::Application.config.secret_token = 'ac352cd961ca7a7fcc40b7a23c72d81de9b7aa40899c040a7f95e525ad717033ca6a068b95628e397800d8fdde0507a81719a3e03255ed5ae01fb214e043aa41'
